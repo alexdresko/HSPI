f7c7a40f0357f93865c304d4c0d6e91b28b3161a
Alex Dresko <me@alexdresko.com>
2017-03-04 10:49:36 -0500

    For template release v1.3.3

---
+++ package-template.ps1
@@ -0,0 +1,16 @@
+﻿.\copy-devtotemplate.ps1
+
+$file = "$PSScriptRoot\Templates\HomeSeerTemplates\source.extension.vsixmanifest"
+
+[xml]$xml = get-content -Path $file
+$version = [System.Version]::Parse($xml.PackageManifest.Metadata.Identity.Version)
+
+$newVersion = "$($version.Major).$($version.Minor).$($version.Build + 1)"
+
+$xml.PackageManifest.Metadata.Identity.Version = $newVersion
+
+$xml.Save($file)
+
+& 'C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe' /t:Build /p:Configuration=Release
+
+"Updated $file to $newVersion"
