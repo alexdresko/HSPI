326a4a72509926c149ad69944e358eaafec1894f
Alex Dresko <me@alexdresko.com>
2017-03-05 16:35:36 -0500

    Strip appveyor version tag

--- update-vsixversion.ps1
+++ update-vsixversion.ps1
@@ -10,6 +10,8 @@ if ($newVersion -eq "") {
     $version = [System.Version]::Parse($xml.PackageManifest.Metadata.Identity.Version)
 
     $newVersion = "$($version.Major).$($version.Minor).$($version.Build + 1)"
+} else {
+    $newVersion = $newVersion -replace "(\d*\.\d*\.\d*\.\d).*", "`$1"
 }
 
 $xml.PackageManifest.Metadata.Identity.Version = $newVersion
