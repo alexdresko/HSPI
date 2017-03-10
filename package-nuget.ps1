8f8a7d3dce00b4f829668c314dce51683a7b6b4b
Alex Dresko <me@alexdresko.com>
2017-03-09 20:51:17 -0500

    Trying to fix the build

--- package-nuget.ps1
+++ package-nuget.ps1
@@ -1,2 +1,2 @@
-﻿nuget pack $PsScriptRoot\hspi -verbosity detailed
+﻿nuget pack $PsScriptRoot\hspi -verbosity detailed -version 1.0.0
 copy $PsScriptRoot\HSPI.1.0.0.nupkg $PsScriptRoot\Templates\HomeSeerTemplates\Packages -Verbose
