b9fc4da4dc514a62b3c3c91d0cdce55e6bf7afee
Alex Dresko <me@alexdresko.com>
2018-08-24 13:48:29 -0400

    Packages updated and builds. But no vsix.

--- HSPI/Options.cs
+++ HSPI/Options.cs
@@ -5,19 +5,10 @@ namespace Hspi
 {
     public class Options
     {
-        [Option('p', "port", HelpText = "HomeSeer admin port", DefaultValue = 10400)]
+        [Option('p', "port", HelpText = "HomeSeer admin port", Default = 10400)]
         public int Port { get; set; } = 10400;
 
-        [Option('s', "server", HelpText = "HomeSeer IP address", DefaultValue = "127.0.0.1")]
+        [Option('s', "server", HelpText = "HomeSeer IP address", Default = "127.0.0.1")]
         public string Server { get; set; } = "127.0.0.1";
-
-        [ParserState]
-        public IParserState LastParserState { get; set; }
-
-        [HelpOption]
-        public string GetUsage()
-        {
-            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
-        }
     }
 }
