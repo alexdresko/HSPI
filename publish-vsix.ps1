fbc6a5027d9de3ed2eb24bfd2defef0822b51e8c
Alex Dresko <me@alexdresko.com>
2018-10-28 12:09:32 -0400

    Might actually publish

---
+++ publish-vsix.ps1
@@ -0,0 +1,17 @@
+$vsixpublish = Get-ChildItem -File .\packages -recurse | 
+    Where-Object { $_.Name -eq "VsixPublisher.exe" } | 
+    Sort-Object -Descending -Property CreationTime | 
+    Select-Object -First 1 -ExpandProperty FullName
+
+. $vsixpublish login -publisherName alexdresko -personalAccessToken $env:homeseertemplatespublish
+
+$overview = (Get-Item .\Templates\HomeSeerTemplates\overview.md).FullName
+$manifest = (Get-Item .\Templates\HomeSeerTemplates\publish-manifest.json).FullName
+$vsix = (Get-Item .\Templates\HomeSeerTemplates\bin\Debug\HomeSeerTemplates.vsix).FullName
+Write-Host "vsix: $vsix"
+Write-Host "manifest: $manifest"
+Write-Host "overview: $overview"
+
+# . $vsixpublish deleteExtension -extensionName "HomeSeerTemplates" -publisherName "thealexdresko"
+
+. $vsixpublish publish -payload "$vsix" -publishManifest "$manifest"
